f449772ef9967c046f0ebbd2e81b1b3e8b70cfe3
Matthew Falcone <mapfalcone@gmail.com>
2025-03-17 13:53:15 +0000

    Improve code style using pylint

--- inflammation/models.py
+++ inflammation/models.py
@@ -28,7 +28,7 @@ def load_csv(filename):
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
 
-    :param data: 2D array with inflammation data (each row indicates a single patient across all days).
+    :param data: 2D array with inflammation data (each row is a single patient across all days).
     :returns: array of mean daily inflammation across all patients.
     """
     return np.mean(data, axis=0)
@@ -37,7 +37,7 @@ def daily_mean(data):
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
 
-    :param data: 2D array with inflammation data (each row indicates a single patient across all days).
+    :param data: 2D array with inflammation data (each row is a single patient across all days).
     :returns: array of daily maximum inflammation across all patients.
     """
     return np.max(data, axis=0)
@@ -46,8 +46,7 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
 
-    :param data: 2D array with inflammation data (each row indicates a single patient across all days).
+    :param data: 2D array with inflammation data (each row is a single patient across all days).
     :returns: array of daily minimum inflammation across all patients.
     """
     return np.min(data, axis=0)
-
